b1420fb5d22a952d6570a4caa04bb2f1329cb336
Samuelthenoob <60633644+Samuelthenoob@users.noreply.github.com>
2020-09-04 17:39:50 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+var image= new Image();
+image.src="https://vignette.wikia.nocookie.net/angrybirds/images/d/d3/INGAME_BLOCKS_WOOD_1.png/revision/latest/window-crop/width/200/x-offset/56/y-offset/0/window-width/391/window-height/390?cb=20110723175423"
+var getImage=function(){
+return image;
+};
